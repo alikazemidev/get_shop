d9352c511d649ae11e2a92d0bb1d87192623060a
Ali kazemi <alikazemidev@gmail.com>
2023-09-16 16:03:34 +0330

    create register screen

---
+++ lib/helpers/widgets/text_field_widget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:get_shop/constants/color.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TextFieldWidget extends StatefulWidget {
+  const TextFieldWidget(
+      {super.key, required this.hintText, this.icon, this.type});
+  final String hintText;
+  final IconData? icon;
+  final TextInputType? type;
+
+  @override
+  State<TextFieldWidget> createState() => _TextFieldWidgetState();
+}
+
+class _TextFieldWidgetState extends State<TextFieldWidget> {
+  bool _obsText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText:
+          widget.type == TextInputType.visiblePassword ? _obsText : false,
+      keyboardType: widget.type == null ? TextInputType.text : widget.type,
+      decoration: InputDecoration(
+        suffixIcon:
+            widget.icon != null || widget.type == TextInputType.visiblePassword
+                ? GestureDetector(
+                    onTap: () {
+                      _obsText = !_obsText;
+                      setState(() {});
+                    },
+                    child: Icon(
+                      widget.type != TextInputType.visiblePassword
+                          ? widget.icon
+                          : (_obsText ? Iconsax.eye : Iconsax.eye_slash),
+                      color: MyColors.hintColor,
+                      size: 20,
+                    ),
+                  )
+                : null,
+        contentPadding: EdgeInsets.symmetric(vertical: 14, horizontal: 20),
+        hintText: widget.hintText,
+        fillColor: Colors.white,
+        filled: true,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(
+            color: Theme.of(context).dividerColor,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(
+            color: Theme.of(context).primaryColor,
+            width: 2,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/modules/auth/screens/register_screen.dart
+++ lib/modules/auth/screens/register_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get_shop/constants/color.dart';
+import 'package:get_shop/helpers/widgets/button_primary.dart';
+import 'package:get_shop/helpers/widgets/text_field_widget.dart';
+import 'package:iconsax/iconsax.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -7,7 +11,92 @@ class RegisterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(child: Text('register screen')),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: 40),
+                  Image.asset(
+                    'assets/images/logo_h.png',
+                    height: 40,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'به فروشگاه آنلاین خودتون خوش اومدید',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: MyColors.darkGreyColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'ثبت نام',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  TextFieldWidget(
+                    hintText: 'نام و نام خانوداگی',
+                    icon: Iconsax.user,
+                  ),
+                  SizedBox(height: 15),
+                  TextFieldWidget(
+                    type: TextInputType.number,
+                    hintText: 'شماره موبایل',
+                    icon: Iconsax.mobile,
+                  ),
+                  SizedBox(height: 15),
+                  TextFieldWidget(
+                    hintText: 'رمز عبور',
+                    type: TextInputType.visiblePassword,
+                  ),
+                  SizedBox(height: 15),
+                  TextFieldWidget(
+                    hintText: 'تکرار رمز عبور',
+                    type: TextInputType.visiblePassword,
+                  ),
+                  SizedBox(height: 35),
+                  ButtonPrimary(
+                    text: 'ثبت نام',
+                    onPressed: () {},
+                  ),
+                  SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'حساب کاربری دارید؟',
+                        style: TextStyle(
+                          color: MyColors.darkGreyColor,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'وارد شوید',
+                          style: TextStyle(
+                            color: MyColors.primaryColor,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
